b4b059c9253b47157c6f973e187fb78f73251e09
Sarah Marshall <marshallsaraha@gmail.com>
2019-11-09 17:17:21 -0800

    Add measurement

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,10 +1,24 @@
 #lang rosette
 
+(require "matrix.rkt")
+
 (provide environment
          interpret-stmt
          interpret-expr)
 
 (struct environment (variables state) #:transparent)
+
+(define identity-operator
+  '((1 0)
+    (0 1)))
+
+(define select-zero
+  '((1 0)
+    (0 0)))
+
+(define select-one
+  '((0 0)
+    (0 1)))
 
 (define (interpret-stmt stmt env)
   (let ([variables (environment-variables env)]
@@ -25,7 +39,7 @@
          (environment (dict-set (environment-variables env*) id value)
                       (environment-state env*)))]
       [`(using (,qubits ...) ,stmts ...)
-       (let* ([next-id (if (empty? state) 0 (log (length state) 2))]
+       (let* ([next-id (if (empty? state) 0 (num-qubits state))]
               [variables* (foldl (lambda (q id vs) (dict-set vs q id))
                            variables
                            qubits
@@ -47,7 +61,30 @@
         [state (environment-state env)])
     (match expr
       [`(x ,q) (error "X gate not implemented")]
-      [`(m ,q) (error "measurement not implemented")]
+      [`(m ,q)
+       (let-values ([(result state*) (measure state (dict-ref variables q))])
+         (values result (environment variables state*)))]
       [`(reset ,q) (error "reset not implemented")]
       [(? boolean?) (values expr env)]
       [id (values (dict-ref variables id) env)])))
+
+(define (apply-to-qubit operator qubit state)
+  (let ([operators (build-list
+                    (num-qubits state)
+                    (lambda (i) (if (= i qubit) operator identity-operator)))])
+    (matrix-multiply (foldl kronecker-product (car operators) (cdr operators))
+                     (list->column-vector state))))
+
+(define (measure state qubit)
+  (let* ([zero-state (apply-to-qubit select-zero qubit state)]
+         [zero-probability (vector-magnitude-sq zero-state)]
+         [one-state (apply-to-qubit select-one qubit state)]
+         [one-probability (vector-magnitude-sq one-state)])
+    ; For now, just choose the result with higher probability.
+    ; TODO: Remember both results somehow.
+    (if (> zero-probability one-probability)
+        (values #f (scale (/ 1 (sqrt zero-probability)) zero-state))
+        (values #t (scale (/ 1 (sqrt one-probability)) one-state)))))
+
+(define (num-qubits state)
+  (exact-truncate (log (length state) 2)))
--- matrix.rkt
+++ matrix.rkt
@@ -1,14 +1,23 @@
 #lang rosette
 
+(provide transpose
+         matrix-conjugate
+         adjoint
+         scale
+         matrix-multiply
+         inner-product
+         vector-magnitude-sq
+         kronecker-product
+         list->column-vector)
+
 (define (transpose a)
   (apply map list a))
 
-(define (conjugate a)
-  (matrix-map (lambda (z) (make-rectangular (real-part z) (- (imag-part z))))
-              a))
+(define (matrix-conjugate a)
+  (matrix-map conjugate a))
 
 (define (adjoint a)
-  (conjugate (transpose a)))
+  (matrix-conjugate (transpose a)))
 
 (define (scale c a)
   (matrix-map (lambda (x) (* c x)) a))
@@ -17,6 +26,13 @@
   (for/list ([a-row a])
     (for/list ([b-row (transpose b)])
       (apply + (map * a-row b-row)))))
+
+(define (inner-product a b)
+  (matrix-multiply (adjoint a) b))
+
+(define (vector-magnitude-sq v)
+  (match (inner-product v v)
+    [`((,x)) x]))
 
 (define (kronecker-product a b)
   (let ([matrix-flatten (lambda (m) (apply map append m))])
