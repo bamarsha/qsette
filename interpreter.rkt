1faa08d98ba39ca18896d373356e6bdaf76474fd
Sarah Marshall <marshallsaraha@gmail.com>
2019-11-12 14:32:00 -0800

    Ignore statements after a return statement

--- interpreter.rkt
+++ interpreter.rkt
@@ -25,45 +25,49 @@
     (0 1)))
 
 (define (interpret-stmt stmt env)
-  ; TODO: Should check if the return value of a recursive interpret-stmt is an
-  ; environment or not (in case of an early return statement).
-  (let ([variables (environment-variables env)]
-        [state (environment-state env)])
-    (match stmt
-      [`(begin ,stmts ...) (foldl interpret-stmt env stmts)]
-      [`(if ,expr ,stmt1 ,stmt2)
-       (let-values ([(value env*) (interpret-expr expr env)])
-         (if value
-             (interpret-stmt stmt1 env*)
-             (interpret-stmt stmt2 env*)))]
-      [`(if ,expr ,stmt1)
-       (interpret-stmt `(if ,expr ,stmt1 (begin)) env)]
-      [`(mutable ,id ,expr)
-       (interpret-stmt `(set ,id ,expr) env)]
-      [`(set ,id ,expr)
-       (let-values ([(value env*) (interpret-expr expr env)])
-         (environment (dict-set (environment-variables env*) id value)
-                      (environment-state env*)))]
-      [`(using (,qubits ...) ,stmts ...)
-       (let* ([next-id (if (empty? state) 0 (num-qubits state))]
-              [variables* (foldl (lambda (q id vs) (dict-set vs q id))
-                           variables
-                           qubits
-                           (stream->list (stream-take (in-naturals next-id)
-                                                      (length qubits))))]
-              [state* (if (empty? state)
-                          (build-list (expt 2 (length qubits))
-                                      (lambda (i) (if (= 0 i) 1 0)))
-                          (append state (build-list (* (length state)
-                                                       (expt 2 (length qubits)))
-                                                    (const 0))))])
-         (foldl interpret-stmt (environment variables* state*) stmts))]
-      [`(return ,expr)
-       (let-values ([(value env) (interpret-expr expr env)])
-         value)]
-      [expr
-       (let-values ([(value env*) (interpret-expr expr env)])
-         env*)])))
+  (if (not (environment? env))
+      ; env is some return value instead, so just return it again.
+      env
+      ; env is actually an environment.
+      (let ([variables (environment-variables env)]
+            [state (environment-state env)])
+        (match stmt
+          [`(begin ,stmts ...) (foldl interpret-stmt env stmts)]
+          [`(if ,expr ,stmt1 ,stmt2)
+           (let-values ([(value env*) (interpret-expr expr env)])
+             (if value
+                 (interpret-stmt stmt1 env*)
+                 (interpret-stmt stmt2 env*)))]
+          [`(if ,expr ,stmt1)
+           (interpret-stmt `(if ,expr ,stmt1 (begin)) env)]
+          [`(mutable ,id ,expr)
+           (interpret-stmt `(set ,id ,expr) env)]
+          [`(set ,id ,expr)
+           (let-values ([(value env*) (interpret-expr expr env)])
+             (environment (dict-set (environment-variables env*) id value)
+                          (environment-state env*)))]
+          [`(using (,qubits ...) ,stmts ...)
+           (let* ([next-id (if (empty? state) 0 (num-qubits state))]
+                  [variables* (foldl (lambda (q id vs) (dict-set vs q id))
+                                     variables
+                                     qubits
+                                     (stream->list
+                                      (stream-take (in-naturals next-id)
+                                                   (length qubits))))]
+                  [state* (if (empty? state)
+                              (build-list (expt 2 (length qubits))
+                                          (lambda (i) (if (= 0 i) 1 0)))
+                              (append state
+                                      (build-list (* (length state)
+                                                     (expt 2 (length qubits)))
+                                                  (const 0))))])
+             (foldl interpret-stmt (environment variables* state*) stmts))]
+          [`(return ,expr)
+           (let-values ([(value env) (interpret-expr expr env)])
+             value)]
+          [expr
+           (let-values ([(value env*) (interpret-expr expr env)])
+             env*)]))))
 
 (define (interpret-expr expr env)
   (let ([variables (environment-variables env)]
