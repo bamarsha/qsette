f490ffa5c66cbb293961d1d1ed081e69e13d2d03
Phillip Quinn <pauqinn@uw.edu>
2019-11-29 15:36:19 -0800

    Change name macro identifier from procedure to operation

--- qsette.rkt
+++ qsette.rkt
@@ -2,20 +2,22 @@
 
 (require "interpreter.rkt")
 
+(provide (all-defined-out))
+         
 (struct qsette (ast proc)
   #:transparent
   #:property prop:procedure
   (struct-field-index proc))
 
 (define (interpret prog inputs)
-  (match-define `(procedure (,_ ,args ...) ,S) prog)
+  (match-define `(operation (,_ ,args ...) ,S) prog)
   (interpret-stmt S (environment (map cons args inputs) (list) (list))))
   
-(define-syntax (procedure stx)
+(define-syntax (operation stx)
   (syntax-case stx ()
     [(_ (id args ...) stmt)
      (syntax/loc stx
        (define id
-         (let* ([ast `(procedure (id args ...) stmt)]
+         (let* ([ast `(operation (id args ...) stmt)]
                 [id (lambda (args ...) (interpret ast (list args ...)))])
            (qsette ast id))))]))
