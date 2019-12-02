66ecc066191b4a5eb25820ef37a8ab44db9b06fc
Phillip Quinn <pauqinn@uw.edu>
2019-12-02 14:55:22 -0800

    Add working loops, evaluated index expressions, bv literals

---
+++ examples/for.rkt
@@ -0,0 +1,22 @@
+#lang rosette
+
+(require "../qsette.rkt")
+
+(operation (for-test)
+           (begin
+             (mutable r #f)
+             (for (i 3)
+               (set r #t))
+             (return r)))
+
+(for-test)
+
+(operation (for-test2)
+           (begin
+             (mutable r #f)
+             (mutable bs (int-as-bool-array ,(bv 4 3) 3))
+             (for (i 3)
+               (set r (index bs i)))
+             (return r)))
+
+(for-test2)
--- examples/measure-integer.rkt
+++ examples/measure-integer.rkt
@@ -42,3 +42,20 @@
 
 (printf "Pr(measure-integer-wrong(n) = n) = 1?\n~a\n"
         (verify (assert (= 1 (probability/v (measure-integer-wrong n) n)))))
+(newline)
+
+(define (measure-integer-loop n)
+  (interpret-stmt '(begin
+                     (mutable r 0)
+                     (mutable bs (int-as-bool-array n 3))
+                     (using ([qs (qubits 3)])
+                            (for (i 3)
+                              (if (index bs i)
+                                  (x (index qs i))))
+                            (set r (measure-integer qs))
+                            (reset-all qs))
+                     (return r))
+                  (environment (list `(n . ,n)) (list) (list))))
+
+(printf "Pr(measure-integer(n) = n) = 1?\n~a\n"
+        (verify (assert (= 1 (probability/v (measure-integer-loop n) n)))))
